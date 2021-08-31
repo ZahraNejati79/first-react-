5c089b23e00d64f88a32735dd662db493a90b668
ZahraNejati <nejatizahra4@gmail.com>
2021-08-31 18:00:48 +0430

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
