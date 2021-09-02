6ababbc52410335b55d8722a569c905565bee489
ZahraNejati <nejatizahra4@gmail.com>
2021-09-02 14:53:14 +0430

    useEffect

---
+++ src/components/FunctionCounter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const FunctionCounter = (props) => {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      ></input>
+      <button onClick={() => setCounter(counter + 1)}>count : {counter}</button>
+    </div>
+  );
+};
+
+export default FunctionCounter;
